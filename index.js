5b8b8f4b691f75ab7af34dd22d800dcf7a6eb497
Samio <samio@samio.co>
2020-09-12 12:30:27 +0000

    routes/todos.js

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var express = require('express'),
 	app = express(),
-	port = process.env.PORT || 3000;
-var todoRoutes = require('./routes/todos');
+	port = process.env.PORT || 3000,
+	bodyParser = require('body-parser'),
+	todoRoutes = require('./routes/todos');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
-	res.send("Hi Root route");
+	res.send("Hi from root route");
 });
 
 app.use('/api/todos', todoRoutes);
 
 app.listen(port, function(){
-   console.log("APP IS RUNNING ON PORT " + port);
+   console.log("APP IS NOW RUNNING ON PORT " + port);
 });
 
